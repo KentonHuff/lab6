b87daf704af88fc77c5adb6b964773d6987ff8c6
Kenton Huff <kentonahuff@gmail.com>
2019-02-25 20:44:12 -0800

    Completed Todo.java

---
+++ Todo.java
@@ -0,0 +1,39 @@
+public class Todo {/*
+Lab Section_________: 11
+Last Modified_______: 2019-02-25
+OU ID_______________: 113411436
+OU EMAIL____________: kentonahuff@ou.edu
+
+Tasks:
+1. Shape Class
+2. Circle class
+3. Polygon
+4. EquilateralTriangle
+5. Trapezoid
+6. Rectangle
+7. Square
+8. Sorter and Comparators
+9. Write all tests
+
+Estimated Time (minutes):
+1. 4
+2. 4
+3. 6
+4. 6
+5. 6
+6. 6
+7. 4
+8. 10
+9. 25
+
+Actual Time (minutes):
+1. 3
+2. 4
+3. 7
+4. 8
+5. 5
+6. 4
+7. 3
+8. 15
+9. 30
+*/}
